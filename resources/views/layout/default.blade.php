4cb5a1332ca2864ca80da4e2fa32816bc5d4e03c
ShenYX0128 <903422096@qq.com>
2020-06-22 09:40:07 +0000

    完成用户注册功能

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -7,7 +7,8 @@
     <body>
 	@include('layout._header')
 	<div class="container">
-	    <div class="offset-md-1 col-md-10">	
+	    <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
 		@yield('content')
 		@include('layout._footer')
 	    </div>
