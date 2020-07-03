a0938ffe1bc31e385c5ed7eeb67cfe1aecbd7d80
ShenYX0128 <903422096@qq.com>
2020-07-03 09:40:30 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->email = '123456@qq.com';
         $user->name = '123456';
+        $user->is_admin = true;
         $user->save();
     }
 }
