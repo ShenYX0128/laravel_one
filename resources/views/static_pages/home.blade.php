750d2bc682c0fa675be53c0e4b2dd511e983030f
ShenYX0128 <903422096@qq.com>
2020-07-24 03:16:14 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,22 @@
 @extends('layout/default')
 @section('title','微博首页')
 @section('content')
-	<a class="btn btn-lg btn-success" role="button" href="{{ route('signup') }}">注册</a>
+    @if(Auth::check())
+        <div class="row">
+            <div class="col-md-8">
+                <section class="status_form">
+                    @include('shared._status_form')
+                </section>
+            </div>
+            <aside class="col-md-4">
+                <section class="user_info">
+                    @include('shared._user_info',['user' => Auth::user()])
+                </section>
+            </aside>
+        </div>
+        @else
+        <a class="btn btn-lg btn-success" role="button" href="{{ route('signup') }}">注册</a>
+    @endif
+
 @stop
 
